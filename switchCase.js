e294aadb5f5b1834f00da0b847b9ea3a11ce35ec
Antonio Vuono <antoniosvuono@icloud.com>
2025-09-04 14:37:17 -0300

    challange switch case developed

--- switchCase.js
+++ switchCase.js
@@ -12,3 +12,41 @@ switch (titleColor) {
     default: 
     console.log("Nenhuma cor definida!")
 }
+
+
+
+
+const handleNotification = (notification) => {
+  const { type, payload } = notification;
+
+  switch (type) { 
+    case 'new_message': 
+     if (payload && payload.senderName) {
+      console.log(`You received a new message from: ${payload.senderName}`);
+     return Alert.alert('New Message', `You have a new message from ${payload.senderName}.`);
+    } 
+
+    console.log('You received a new message.');
+    break;
+    case 'promotion':
+    if (payload && payload.url) {
+      console.log(`Opening promotion link: ${payload.url}`);
+     return Linking.openURL(payload.url);
+    } 
+   
+    console.log('Received a promotion notification without a link.');
+    break;
+    case 'app_update': 
+    if (payload && payload.version) {
+      console.log(`A new app version is available: ${payload.version}`);
+      return Alert.alert('Update Available', 'A new app update is available. Please update now.');
+    } 
+
+    console.log('App update notification received.');
+    break;
+    default: 
+    console.log(`Received an unknown notification type: ${type}`);
+  }
+}
+
+handleNotification(userLoginPayload);
