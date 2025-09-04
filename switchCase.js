18b6b208e7e5efe16dd179caaf2f50d9032918c4
Antonio Vuono <antoniosvuono@icloud.com>
2025-09-04 12:45:38 -0300

    switch case studie

---
+++ switchCase.js
@@ -0,0 +1,14 @@
+const titleColor = "primary1"
+
+switch (titleColor) {
+    case 'primary': 
+    console.log("Fundo amarelo")
+    break;
+    case 'secondary': 
+    console.log("Fundo verde")
+    break;
+    case 'thirdiary':
+    console.log("cor vermelha")
+    default: 
+    console.log("Nenhuma cor definida!")
+}
